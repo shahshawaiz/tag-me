6c3f3970db67edc2b909534987ed255122211240
unknown <shahshawaiz@gmail.com>
2019-02-08 00:37:59 +0100

    api post requesst

--- Analyzer.py
+++ Analyzer.py
@@ -7,50 +7,15 @@ import tensorflow as tf
 from PIL import Image
 from utils import label_map_util
 from utils import visualization_utils as vis_util
+import time
+import urllib.request as urllibDownloads
 
-# constants for model name and remote path for downloadg 
+# vars: constants for model name and remote path for downloadg 
 MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
 MODEL_FILE = MODEL_NAME + '.tar.gz'
 DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
-
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
-
-# init detection graph script
-detection_graph = tf.Graph()
-
-# get detault graph
-with detection_graph.as_default():
-
-  # get ref to graph def
-  od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid: # read frozen graph in rb mode 
-
-    # serialized frozen graph 
-    serialized_graph = fid.read()
-
-    # parse graph def
-    od_graph_def.ParseFromString(serialized_graph) # copy frozen graph meta to prior initliazed tf def
-    tf.import_graph_def(od_graph_def, name='') # import graph to tf session
-
-# init url opener
-opener = urllib.request.URLopener()
-
-# download base file
-opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-
-# get tar handle
-tar_file = tarfile.open(MODEL_FILE)
-
-# iterate tar items
-for file in tar_file.getmembers():
-
-  # get filename
-  file_name = os.path.basename(file.name)
-
-  # if frozen_inference_graph then extract 
-  if 'frozen_inference_graph.pb' in file_name:
-    tar_file.extract(file, os.getcwd())
+PATH_TO_TEST_IMAGES_DIR = 'images'
 
 # load image to np
 def load_image_into_numpy_array(image):
@@ -61,6 +26,52 @@ def load_image_into_numpy_array(image):
   # init np with same dim and copy
   return np.array(image.getdata()).reshape(
       (im_height, im_width, 3)).astype(np.uint8)
+
+# download frozen graph
+def download_model():
+
+  # check forzen model exists 
+  if( not os.path.isdir(MODEL_NAME) ):
+    
+    # init url opener
+    opener = urllib.request.URLopener()
+
+    # download base file
+    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+
+    # get tar handle
+    tar_file = tarfile.open(MODEL_FILE)
+
+    # iterate tar items
+    for file in tar_file.getmembers():
+
+      # get filename
+      file_name = os.path.basename(file.name)
+
+      # if frozen_inference_graph then extract 
+      if 'frozen_inference_graph.pb' in file_name:
+        tar_file.extract(file, os.getcwd())
+
+def get_detection_graph():
+
+  # init tensor graph
+  detection_graph = tf.Graph()
+
+  # get detault graph
+  with detection_graph.as_default():
+
+    # get ref to graph def
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid: # read frozen graph in rb mode 
+
+      # serialized frozen graph 
+      serialized_graph = fid.read()
+
+      # parse graph def
+      od_graph_def.ParseFromString(serialized_graph) # copy frozen graph meta to prior initliazed tf def
+      tf.import_graph_def(od_graph_def, name='') # import graph to tf session
+
+  return detection_graph
 
 # analyze image
 def run_inference_for_single_image(image, graph):
@@ -104,7 +115,28 @@ def run_inference_for_single_image(image, graph):
 
   return output_dict
 
-def demo(image_np):
+def predict_label(image_url):
+
+  # download model
+  download_model()
+
+  # get detection graph
+  detection_graph = get_detection_graph()
+
+  # download image
+  image_filename = str(time.time())
+  urllibDownloads.urlretrieve(image_url, 
+    os.path.join(
+      PATH_TO_TEST_IMAGES_DIR, image_filename
+    ) 
+  )
+
+  # get image handle
+  image_path = os.path.join(PATH_TO_TEST_IMAGES_DIR, image_filename)
+  image = Image.open(image_path)
+
+  # convert to numpy
+  image_np = load_image_into_numpy_array(image)
 
   # List of the strings that is used to add correct label for each box.
   PATH_TO_LABELS = os.path.join('object_detection', 'data', 'mscoco_label_map.pbtxt')
@@ -126,12 +158,4 @@ def demo(image_np):
       use_normalized_coordinates=True,
       line_thickness=8)
 
-  print(labels)
-
-# get image
-PATH_TO_TEST_IMAGES_DIR = 'test_images'
-image_path = os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image2.jpg')
-image = Image.open(image_path)
-image_np = load_image_into_numpy_array(image)
-
-demo(image_np)
+  return labels
